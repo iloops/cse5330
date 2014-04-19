a97c3891ff20b7366f2441d744d075e1bdf664e6
iloops <iloops.jose@gmail.com>
2014-04-18 23:04:05 -0500

    Updated Banner
    Updated index.php with customized banner and css

--- index.php
+++ index.php
@@ -4,33 +4,36 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Public Health Surveilance</title>
+<link rel="stylesheet" type="text/css" href="gcss.css">
+<meta charset="UTF-8">
+
+<title>Public Health Surveilance</title>
 </head>
-<body>
+<body style="">
+<header>
+  <div> <a href="index.php"> <img src="images/banner.png" alt="Welcome to Public Health Surveilance"></a> </div>
+</header>
 
-<HEADER>
-    <div>
-        <blockquote> "CSE5330"</blockquote> 
-        <img src="">
-    </div>
-
-</HEADER>
-<CONTENT>
-    <table>
-        <blockquote> "Welcome"</blockquote> 
-    </table>
-
-</CONTENT>
+<nav> <a href="login.php">login</a> | <a href="survey.php">Survey</a> | <a href="analitics.php">Analytics</a> | <a href="links.php">other</a> </nav>
+<section>
+<h1>Welcome</h1>
+  <p>Public Health Surveilace is a place where we can store, retrieve and analyze citizens data.</p>
+</section>
+<article>
+ <h3>Class Project</h3>
+  <p>This is for a class project, directed for team work, with emphasis on database design.</p>
+</article>
 <aside>
-    <ul>
-        <li>point 1</li>
-        <li>point 2</li>
-    </ul>
+What would you like to do today?
+  <ul>
+    <li><a href="module1">Conduct a survey</a></li>
+    <li><a href="module2">Analyze data</a></li>
+  </ul>
 </aside>
-
-
 </body>
 <footer>
-    <address> 123 UTA blvd, Arlington Texas. 76019</address>
+  <address>
+  123 UTA blvd, Arlington Texas. 76019
+  </address>
 </footer>
 </html>
